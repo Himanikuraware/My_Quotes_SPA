58b461026a46830eb0ab248b394b15a35d96de5c
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-19 23:23:06 +0530

    Completed the comment section.

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,53 @@
-import { useRef } from 'react';
+import { useEffect, useState } from "react";
 
-import classes from './NewCommentForm.module.css';
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
-  const commentTextRef = useRef();
+  const [comment, setComment] = useState();
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+    sendRequest({
+      commentData: { text: comment },
+      quoteId: props.quoteId,
+    });
+    setComment("");
+  };
 
-    // optional: Could validate here
-
-    // send comment to server
+  const commentHandler = (event) => {
+    setComment(event.target.value);
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea
+          id="comment"
+          rows="5"
+          value={comment}
+          onChange={commentHandler}
+        />
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
