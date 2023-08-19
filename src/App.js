915073115fb18a4093d09b22ec8e93a29547f160
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-19 17:35:20 +0530

    Added Routes and pages.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Switch, Route } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuoteDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuoteDetail = () => {
+  return <div>QuoteDetail</div>;
+};
+
+export default QuoteDetail;
