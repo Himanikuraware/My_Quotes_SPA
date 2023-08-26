92b89a3e9bd9861d84d1236a70e86ee19873adbd
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-26 13:51:37 +0530

    Added Lazy loading.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,60 @@
-import React, { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
 import AuthPage from "./pages/AuthPage";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
 import AuthContext from "./store/auth-context";
+
+// Lazy load the pages.
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   const authCtx = useContext(AuthContext);
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        {!authCtx.isLoggedIn && (
-          <Route path="/auth" exact>
-            <AuthPage />
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
           </Route>
-        )}
-        <Route path="/profile" exact>
-          {authCtx.isLoggedIn && <UserProfile />}
-          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          {authCtx.isLoggedIn && <NewQuote />}
-          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
-        </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          {!authCtx.isLoggedIn && (
+            <Route path="/auth" exact>
+              <AuthPage />
+            </Route>
+          )}
+          <Route path="/profile" exact>
+            {authCtx.isLoggedIn && <UserProfile />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            {authCtx.isLoggedIn && <NewQuote />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
 
-        {/* '*'  means except above routes all the routes*/}
-        
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+          {/* '*'  means except above routes all the routes*/}
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
